472187c330eea8c655da9f2af53f71ed36eb2dcd
Mikail Syed <mikailsyed@yahoo.com>
2026-04-12 13:31:15 -0400

    if struct

--- graph.h
+++ graph.h
@@ -1,6 +1,13 @@
 // Header file for graph
 
+
+typedef struct interface {
+	char interfaceName[INTERFACE_NAME_SIZE];
+	struct node *attrNode;
+	struct link *link;
+} interface_t;
+
 typedef struct graph {
 	char topologyName[32];
-
+	linkedlist_t nodeList; // implement this as glued linked list in separate files
 };
