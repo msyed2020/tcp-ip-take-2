ad7ec2b06d8d306ea7dc107ceb46e6b5b36b4717
Mikail Syed <mikailsyed@yahoo.com>
2026-04-14 13:21:50 -0400

    graphing

--- graph.c
+++ graph.c
@@ -12,4 +12,8 @@ graph_t* buildFirstTopo() {
 	node_t* R2_re = createGraphNode(topo, "R2_re");
 
 	insertLinkBetweenTwoNodes(R0_re, R1_re, "eth0/0", "eth0/1", 1);
+
+	insertLinkBetweenTwoNodes(R1_re, R2_re, "eth0/2", "eth0/3", 1);
+
+	insertLinkBetweenTwoNodes(R0_re, R2_re, "eth0/4", "eth0/5", 1);
 }
