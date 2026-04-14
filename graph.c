3bff0f573da0d75fb8cfcada4eb31f9e5bb9d003
Mikail Syed <mikailsyed@yahoo.com>
2026-04-14 13:20:37 -0400

    graphing

--- graph.c
+++ graph.c
@@ -3,7 +3,13 @@
 #include "graph.h"
 
 graph_t* buildFirstTopo() {
-	graph_t* topo = createNewGraph("Generic Graph");
+	graph_t *topo = createNewGraph("Generic Graph");
 
+	node_t* R0_re = createGraphNode(topo, "R0_re");
+	
+	node_t* R1_re = createGraphNode(topo, "R1_re");
 
+	node_t* R2_re = createGraphNode(topo, "R2_re");
+
+	insertLinkBetweenTwoNodes(R0_re, R1_re, "eth0/0", "eth0/1", 1);
 }
