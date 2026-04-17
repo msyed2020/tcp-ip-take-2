c91d1eda6ae05e5c9bbfadcbd194653e5597efe3
Mikail Syed <mikailsyed@yahoo.com>
2026-04-17 13:29:46 -0400

    d

--- net.c
+++ net.c
@@ -3,3 +3,11 @@
 #include "utils.h"
 #include <stdio.h>
 
+// assigning unique MAC address to an interface in the graph
+
+void interfaceAssignMACAddress(interface_t* interface) {
+	memset(IF_MAC(interface), 0, 8);
+	strcpy(IF_MAC(interface), interface->attachedNode->nodeName);
+	strcat(IF_MAC(interface), interface->interfaceName);
+}
+
