b79fb3487a74b2d60c60e1e9e8230d06843f8e20
Mikail Syed <mikailsyed@yahoo.com>
2026-04-17 13:33:16 -0400

    d

--- net.c
+++ net.c
@@ -11,3 +11,14 @@ void interfaceAssignMACAddress(interface_t* interface) {
 	strcat(IF_MAC(interface), interface->interfaceName);
 }
 
+// seeing if loopback address can be properly configured for a node
+
+bool_t nodeSetLoopbackAddress(node_t* node, char* ipAddr) {
+	assert(ipAddr);
+
+	node->nodeNetworkProperties.isAddressLoopback = true;
+	strncpy(NODE_LO_ADDR(node), ipAddr, 16);
+
+
+	return true;
+}
